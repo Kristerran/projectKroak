96aa5cac9142f637c62f1df6e5c97f4766a81542
Kris Baily <kristopherbaily@gmail.com>
2021-10-30 14:31:00 -0700

    Updated seeds and added preliminary save function

--- public/javascript/virtuefig.js
+++ public/javascript/virtuefig.js
@@ -1,16 +1,14 @@
 const imgHolder = document.getElementById('viewHolder')
-
+const saveHolder = []
 function changeFunc(htmlObject) {
     console.log(htmlObject)
     var selectedValue = JSON.parse(htmlObject.value);
-    console.log(selectedValue)
-    console.log(selectedValue.imgSrc)
-    console.log(selectedValue.docId)
+    saveHolder[htmlObject.slot] = {}
+    saveHolder[htmlObject.slot].imgSrc = selectedValue.imgSrc
+    saveHolder[htmlObject.slot].docId = selectedValue.docId
+    saveHolder[htmlObject.slot].name = selectedValue.innerhtml
     var imageElement = document.getElementById(selectedValue.docId);
     imageElement.src = selectedValue.imgSrc;
+    console.log(saveHolder)
    }
 
-//    [
-//     {'imgSrc' : '{{this.image_src}}'},
-//     {'docId' : '{{this.question_id}}'}
-//     ]
