0d83a9c4bb5d4ff51669ccbc5275c3728d1a4c3f
Kris Baily <kristopherbaily@gmail.com>
2021-10-30 14:41:42 -0700

    Cleaned up console.logs

--- public/javascript/virtuefig.js
+++ public/javascript/virtuefig.js
@@ -1,14 +1,11 @@
-const imgHolder = document.getElementById('viewHolder')
 const saveHolder = []
 function changeFunc(htmlObject) {
-    console.log(htmlObject)
-    var selectedValue = JSON.parse(htmlObject.value);
+    let selectedValue = JSON.parse(htmlObject.value);
+    let imageElement = document.getElementById(selectedValue.docId);
     saveHolder[htmlObject.slot] = {}
     saveHolder[htmlObject.slot].imgSrc = selectedValue.imgSrc
     saveHolder[htmlObject.slot].docId = selectedValue.docId
     saveHolder[htmlObject.slot].name = selectedValue.innerhtml
-    var imageElement = document.getElementById(selectedValue.docId);
     imageElement.src = selectedValue.imgSrc;
-    console.log(saveHolder)
    }
 
